0453f197ac70f0e4262d59dc1e35fa0134feebc7
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2024-01-10 18:16:48 +0530

    Backend completed

--- server/index.js
+++ server/index.js
@@ -8,12 +8,20 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { error } from "console";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
-/*CONFIGURATION*/
-const __fileName = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__fileName);
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -23,25 +31,27 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-/* FILE STORAGE*/
+/* FILE STORAGE */
 const storage = multer.diskStorage({
-    destination: function (req, file, ch) {
+    destination: function (req, file, cb) {
         cb(null, "public/assets");
     },
-    filename: function (req, file, ch) {
+    filename: function (req, file, cb) {
         cb(null, file.originalname);
-    }
+    },
 });
 const upload = multer({ storage });
 
-/* ROUTES WITH FILES*/
+/* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-/*ROUTES*/
-// app.use("/auth", authroutes)
-
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // Health check route
 app.get('/health', (req, res) => {
